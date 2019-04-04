32af21957bbf45f0ae215ad980915084df59a4f4
acch2016 <ie696846@iteso.mx>
2019-04-03 22:50:50 -0600

    correccionesNico10pm

--- freertos_hello.c
+++ freertos_hello.c
@@ -40,8 +40,14 @@
 #include "fsl_flexcan.h"
 #include "board.h"
 
+#include "fsl_uart_freertos.h" //lin
+#include "fsl_uart.h" //lin
+
 #include "pin_mux.h"
 #include "clock_config.h"
+
+#include "lin1d3_driver.h" //lin
+#include "FreeRTOSConfig.h" //lin
 
 #include "ADC.h"
 #include "GPIO.h"
@@ -62,12 +68,30 @@
 #define TX100_MESSAGE_BUFFER_NUM (8)
 #define TX50_MESSAGE_BUFFER_NUM (7)
 #define CAN2
+/* UART instance and clock */
+#define MASTER_UART UART3
+#define MASTER_UART_CLKSRC UART3_CLK_SRC
+#define MASTER_UART_CLK_FREQ CLOCK_GetFreq(UART3_CLK_SRC)
+#define MASTER_UART_RX_TX_IRQn UART3_RX_TX_IRQn
+/* Task priorities. */
+#define init_task_PRIORITY (configMAX_PRIORITIES - 2)
+#define test_task_heap_size_d	(192)
+
+#define app_message_id_1_d (0x01<<2|message_size_2_bytes_d)
+#define app_message_id_2_d (0x02<<2|message_size_4_bytes_d)
+#define app_message_id_3_d (0x03<<2|message_size_8_bytes_d)
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
 static void task_100ms(void *pvParameters);
 static void task_50ms(void *pvParameters);
 static void task_rx(void *pvParameters);
+
+//static void test_task(void *pvParameters); //lin
+
+static void	message_1_callback_master(void* message); //lin
+static void	message_2_callback_master(void* message); //lin
+static void	message_3_callback_master(void* message); //lin
 /*******************************************************************************
  * Variables
  ******************************************************************************/
@@ -93,12 +117,15 @@ volatile TickType_t xFrequency = OS_TICK_PERIOD_50MS;
 
 uint8_t adcLIN_L = 0;
 uint8_t adcLIN_H = 0;
+volatile bool lin_flag = false;
+volatile lin1d3_handle_t* master_handle;
 /*******************************************************************************
  * Code
  ******************************************************************************/
 /*!
  * @brief Application entry point.
  */
+
 /*!
  * @brief FlexCAN Call Back function
  */
@@ -176,6 +203,8 @@ int main(void)
 	BOARD_InitPins();
 	BOARD_BootClockRUN();
 	BOARD_InitDebugConsole();
+	NVIC_SetPriority(MASTER_UART_RX_TX_IRQn, 5); //lin
+
 	CAN_Init();
 
     xTaskCreate(task_100ms, "100ms Task", configMINIMAL_STACK_SIZE + 10, NULL, hello_task_PRIORITY, NULL);
@@ -184,6 +213,13 @@ int main(void)
     xTaskCreate(task_50ms, "50ms Task", configMINIMAL_STACK_SIZE + 10, NULL, hello_task_PRIORITY, NULL);
 #endif
     xTaskCreate(task_rx, "rx Task", configMINIMAL_STACK_SIZE + 10, NULL, hello_task_PRIORITY, NULL);
+
+//    if (xTaskCreate(test_task, "test_task", test_task_heap_size_d, NULL, init_task_PRIORITY, NULL) != pdPASS)
+//    {
+//        PRINTF("Init Task creation failed!.\r\n");
+//        while (1)
+//            ;
+//    }
     vTaskStartScheduler();
 
     for (;;)
@@ -200,18 +236,53 @@ static void task_100ms(void *pvParameters)
 
 	volatile uint32_t can_flags = 0;
 
+	int error;
+	lin1d3_nodeConfig_t node_config;
+	lin1d3_handle_t* master_handle;
+	/* Set Master Config */
+	node_config.type = lin1d3_master_nodeType;
+	node_config.bitrate = 9600;
+	node_config.uartBase = MASTER_UART;
+	node_config.srcclk = MASTER_UART_CLK_FREQ;
+	memset(node_config.messageTable,0, (sizeof(node_config.messageTable[0])*lin1d3_max_supported_messages_per_node_cfg_d));
+	node_config.messageTable[0].ID = app_message_id_1_d;
+	node_config.messageTable[0].handler = message_1_callback_master;
+	node_config.messageTable[1].ID = app_message_id_2_d;
+	node_config.messageTable[1].handler = message_2_callback_master;
+	node_config.messageTable[2].ID = app_message_id_3_d;
+	node_config.messageTable[2].handler = message_3_callback_master;
+	/* Init Master node */
+	master_handle = lin1d3_InitNode(node_config);
+	if((NULL == master_handle))
+	{
+		error = kStatus_Fail;
+	} else {
+		error = kStatus_Success;
+	}
+
 	// Initialize the xLastWakeTime variable with the current time.
 	xLastWakeTime = xTaskGetTickCount();
     for (;;)
     {
+    	if(0 == xFrequency_LIN)
+    	{
+    		vTaskDelay(10);
+    		continue;
+    	}
     	/* Get the flags from the can Driver */
     	can_flags = FLEXCAN_GetStatusFlags(EXAMPLE_CAN);
-    	adcLIN_L = 200;
-    	adcLIN_H = 0;
-    	PRINTF("___________________request ADC LIN\r\n");
-//    	lin1d3_masterSendMessage(master_handle, app_message_id_1_d);
-//    	wait for response from master
+    	/** Transmitting by CAN to panel from global variables uint8
+    	    	that will store what we'll get from callback 1
+    	    	TODO bit shift **/
 
+    	PRINTF("___________________request ADC LIN\r\n");
+    	lin1d3_masterSendMessage(master_handle, app_message_id_1_d);
+    	/** Waiting for response from master **/
+    	while(false == lin_flag)
+    	{
+    		vTaskDelay(2);
+    	}
+    	lin_flag = false;
     	/* Send a Can message */
     	tx100Frame.id = FLEXCAN_ID_STD(tx2Identifier);
     	tx100Frame.format = kFLEXCAN_FrameFormatStandard;
@@ -222,11 +293,6 @@ static void task_100ms(void *pvParameters)
         tx100Frame.dataByte0 = adcLIN_L;
     	tx100Frame.dataByte1 = adcLIN_H;
     	FLEXCAN_TransferSendNonBlocking(EXAMPLE_CAN, &flexcanHandle, &tx100Xfer);
-
-
-//    	Transmit by CAN to panel from global variables uint8
-//    	that will store what we'll get from callback
-
 
         // Wait for the next cycle.
         vTaskDelayUntil( &xLastWakeTime, xFrequency_LIN);
@@ -264,6 +330,11 @@ static void task_50ms(void *pvParameters)
 	xLastWakeTime = xTaskGetTickCount();
     for (;;)
     {
+    	if(0 == xFrequency)
+    	{
+    		vTaskDelay(10);
+    		continue;
+    	}
     	/* Get the flags from the can Driver */
     	can_flags = FLEXCAN_GetStatusFlags(EXAMPLE_CAN);
 
@@ -381,21 +452,21 @@ static void task_rx(void *pvParameters)
     			if (rxFrame->dataByte0 & 0x04)
     			{
     				PRINTF("led LIN on");
-//    				lin1d3_masterSendMessage(master_handle, app_message_id_2_d);
+    				lin1d3_masterSendMessage(master_handle, app_message_id_2_d);
 				}
     			else
     			{
     				PRINTF("led LIN off");
-//    				lin1d3_masterSendMessage(master_handle, app_message_id_3_d);
+    				lin1d3_masterSendMessage(master_handle, app_message_id_3_d);
 				}
     		}
     		else if((rxFrame->id>>18) == 0x11)
 			{
-    			if ((rxFrame->dataByte1) > 0)
+//    			if ((rxFrame->dataByte1) > 0)
     			{
     				xFrequency = (rxFrame->dataByte1)*100;
 				}
-    			if ((rxFrame->dataByte2) > 0)
+//    			if ((rxFrame->dataByte2) > 0)
     			{
     				xFrequency_LIN = (rxFrame->dataByte2)*100;
 				}
@@ -405,4 +476,64 @@ static void task_rx(void *pvParameters)
     	}
     	vTaskDelay(10);//2
     }
+}
+
+//static void test_task(void *pvParameters)
+//{
+//	int error;
+//	lin1d3_nodeConfig_t node_config;
+////	lin1d3_handle_t* master_handle;
+//	/* Set Master Config */
+//	node_config.type = lin1d3_master_nodeType;
+//	node_config.bitrate = 9600;
+//	node_config.uartBase = MASTER_UART;
+//	node_config.srcclk = MASTER_UART_CLK_FREQ;
+//	memset(node_config.messageTable,0, (sizeof(node_config.messageTable[0])*lin1d3_max_supported_messages_per_node_cfg_d));
+//	node_config.messageTable[0].ID = app_message_id_1_d;
+//	node_config.messageTable[0].handler = message_1_callback_master;
+//	node_config.messageTable[1].ID = app_message_id_2_d;
+//	node_config.messageTable[1].handler = message_2_callback_master;
+//	node_config.messageTable[2].ID = app_message_id_3_d;
+//	node_config.messageTable[2].handler = message_3_callback_master;
+//	/* Init Master node */
+//	master_handle = lin1d3_InitNode(node_config);
+//	if((NULL == master_handle))
+//	{
+//		error = kStatus_Fail;
+//	} else {
+//		error = kStatus_Success;
+//	}
+//
+//	while (kStatus_Success == error) {
+//		vTaskDelay(200);
+//		lin1d3_masterSendMessage(master_handle, app_message_id_1_d);
+//		vTaskDelay(200);
+//		lin1d3_masterSendMessage(master_handle, app_message_id_2_d);
+//		vTaskDelay(200);
+//		lin1d3_masterSendMessage(master_handle, app_message_id_3_d);
+//	}
+//
+//	vTaskSuspend(NULL);
+//}
+
+static void	message_1_callback_master(void* message)
+{
+	uint8_t* message_data = (uint8_t*)message;
+	adcLIN_L = message_data[0];
+	adcLIN_H = message_data[1];
+	uint16_t adc_value = message_data[0] | (message_data[1] << 8);
+	PRINTF("Master got response to message 1 ADC value: %d\r\n", adc_value);
+	lin_flag  = true;
+}
+
+static void	message_2_callback_master(void* message)
+{
+	uint8_t* message_data = (uint8_t*)message;
+	PRINTF("Master got response to message 2 %d\r\n", message_data[0]);
+}
+static void	message_3_callback_master(void* message)
+{
+	uint8_t* message_data = (uint8_t*)message;
+	PRINTF("Master got response to message 3 %d\r\n", message_data[0]);
+
 }
